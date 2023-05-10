cc7dcf42c44f6488d4621efd0d702bf3bec0758a
Riccardo Leone <riccardoleone1991@gmail.com>
2023-05-10 17:28:12 +0200

    Added phenomenological hopf model

---
+++ neurolib/models/pheno_hopf/loadDefaultParams.py
@@ -0,0 +1,95 @@
+import numpy as np
+
+from ...utils.collections import dotdict
+
+
+def loadDefaultParams(Cmat=None, Dmat=None, seed=None):
+    """Load default parameters for the Hopf model
+
+    :param Cmat: Structural connectivity matrix (adjacency matrix) of coupling strengths, will be normalized to 1. If not given, then a single node simulation will be assumed, defaults to None
+    :type Cmat: numpy.ndarray, optional
+    :param Dmat: Fiber length matrix, will be used for computing the delay matrix together with the signal transmission speed parameter `signalV`, defaults to None
+    :type Dmat: numpy.ndarray, optional
+    :param seed: Seed for the random number generator, defaults to None
+    :type seed: int, optional
+
+    :return: A dictionary with the default parameters of the model
+    :rtype: dict
+    """
+
+    params = dotdict({})
+
+    ### runtime parameters
+    params.dt = 0.1  # ms 0.1ms is reasonable
+    params.duration = 2000  # Simulation duration (ms)
+    np.random.seed(seed)  # seed for RNG of noise and ICs
+    # set seed to 0 if None, pypet will complain otherwise
+    params.seed = seed or 0
+
+    # make sure that seed=0 remains None
+    if seed == 0:
+        seed = None
+
+    # ------------------------------------------------------------------------
+    # global whole-brain network parameters
+    # ------------------------------------------------------------------------
+
+    # the coupling parameter determines how nodes are coupled.
+    # "diffusive" for diffusive coupling, "additive" for additive coupling
+    params.coupling = "diffusive"
+
+    params.signalV = 20.0
+    params.K_gl = 0.6  # global coupling strength
+
+    if Cmat is None:
+        params.N = 1
+        params.Cmat = np.zeros((1, 1))
+        params.lengthMat = np.zeros((1, 1))
+
+    else:
+        params.Cmat = Cmat.copy()  # coupling matrix
+        np.fill_diagonal(params.Cmat, 0)  # no self connections
+        params.N = len(params.Cmat)  # number of nodes
+        params.lengthMat = Dmat
+
+    # ------------------------------------------------------------------------
+    # local node parameters
+    # ------------------------------------------------------------------------
+
+    # external input parameters:
+    params.sigma = 0.0  # mV/ms/sqrt(ms) noise intensity
+
+    # neural mass model parameters
+    params.a = 0.25 * np.ones(params.N)  # Hopf bifurcation parameter
+    params.w = 0.2 * np.ones(params.N) # Oscillator frequency, 32 Hz at w = 0.2
+
+    # ------------------------------------------------------------------------
+
+    # initial values of the state variables
+    params.xs_init = 0.5 * np.random.uniform(-1, 1, (params.N, 1))
+    params.ys_init = 0.5 * np.random.uniform(-1, 1, (params.N, 1))
+
+    # values of the external inputs
+    params.x_ext = np.zeros((params.N,))
+    params.y_ext = np.zeros((params.N,))
+
+    return params
+
+
+def computeDelayMatrix(lengthMat, signalV, segmentLength=1):
+    """Compute the delay matrix from the fiber length matrix and the signal velocity
+
+    :param lengthMat:       A matrix containing the connection length in segment
+    :param signalV:         Signal velocity in m/s
+    :param segmentLength:   Length of a single segment in mm
+
+    :returns:    A matrix of connexion delay in ms
+    """
+
+    normalizedLenMat = lengthMat * segmentLength
+    # Interareal connection delays, Dmat(i,j) in ms
+    if signalV > 0:
+        Dmat = normalizedLenMat / signalV
+    else:
+        Dmat = lengthMat * 0.0
+    return Dmat
---
+++ neurolib/models/pheno_hopf/timeIntegration.py
@@ -0,0 +1,210 @@
+import numpy as np
+import numba
+
+from . import loadDefaultParams as dp
+from ...utils import model_utils as mu
+
+
+def timeIntegration(params):
+    """Sets up the parameters for time integration
+    
+    :param params: Parameter dictionary of the model
+    :type params: dict
+    :return: Integrated activity variables of the model
+    :rtype: (numpy.ndarray,)
+    """
+
+    dt = params["dt"]  # Time step for the Euler intergration (ms)
+    duration = params["duration"]  # imulation duration (ms)
+    RNGseed = params["seed"]  # seed for RNG
+
+    # ------------------------------------------------------------------------
+    # local parameters
+    a = params["a"]  # Hopf bifurcation parameter
+    w = params["w"]  # Oscillator frequency
+
+    # external input parameters:
+    # Parameter of the gaussian white noise
+    sigma = params["sigma"]
+
+
+    # ------------------------------------------------------------------------
+    # global coupling parameters
+
+    # Connectivity matrix
+    # Interareal relative coupling strengths (values between 0 and 1), Cmat(i,j) connnection from jth to ith
+    Cmat = params["Cmat"]
+    N = len(Cmat)  # Number of nodes
+    K_gl = params["K_gl"]  # global coupling strength
+    # Interareal connection delay
+    lengthMat = params["lengthMat"]
+    signalV = params["signalV"]
+
+    # Additive or diffusive coupling scheme
+    coupling = params["coupling"]
+    # convert to integer for faster integration later
+    if coupling == "diffusive":
+        coupling = 0
+    elif coupling == "additive":
+        coupling = 1
+    else:
+        raise ValueError('Paramter "coupling" must be either "diffusive" or "additive"')
+
+    if N == 1:
+        Dmat = np.zeros((N, N))
+    else:
+        # Interareal connection delays, Dmat(i,j) Connnection from jth node to ith (ms)
+        Dmat = dp.computeDelayMatrix(lengthMat, signalV)
+        Dmat[np.eye(len(Dmat)) == 1] = np.zeros(len(Dmat))
+    Dmat_ndt = np.around(Dmat / dt).astype(int)  # delay matrix in multiples of dt
+    params["Dmat_ndt"] = Dmat_ndt
+    # ------------------------------------------------------------------------
+
+    # Initialization
+    # Floating point issue in np.arange() workaraound: use integers in np.arange()
+    t = np.arange(1, round(duration, 6) / dt + 1) * dt  # Time variable (ms)
+
+    sqrt_dt = np.sqrt(dt)
+
+    max_global_delay = np.max(Dmat_ndt)
+    startind = int(max_global_delay + 1)  # timestep to start integration at
+
+    # state variable arrays, have length of t + startind
+    # they store initial conditions AND simulated data
+    xs = np.zeros((N, startind + len(t)))
+    ys = np.zeros((N, startind + len(t)))
+
+    x_ext = mu.adjustArrayShape(params["x_ext"], xs)
+    y_ext = mu.adjustArrayShape(params["y_ext"], ys)
+
+    # ------------------------------------------------------------------------
+    # Set initial values
+    # if initial values are just a Nx1 array
+    if np.shape(params["xs_init"])[1] == 1:
+        xs_init = np.dot(params["xs_init"], np.ones((1, startind)))
+        ys_init = np.dot(params["ys_init"], np.ones((1, startind)))
+    # if initial values are a Nxt array
+    else:
+        xs_init = params["xs_init"][:, -startind:]
+        ys_init = params["ys_init"][:, -startind:]
+
+    # xsd = np.zeros((N,N))  # delayed activity
+    xs_input_d = np.zeros(N)  # delayed input to x
+    ys_input_d = np.zeros(N)  # delayed input to y
+
+    np.random.seed(RNGseed)
+
+    # Save the noise in the activity array to save memory
+    xs[:, startind:] = np.zeros((N, len(t))) #np.random.standard_normal((N, len(t)))
+    ys[:, startind:] = np.zeros((N, len(t))) #np.random.standard_normal((N, len(t)))
+
+    xs[:, :startind] = xs_init
+    ys[:, :startind] = ys_init
+
+    noise_xs = np.zeros((N,))
+    noise_ys = np.zeros((N,))
+
+    # ------------------------------------------------------------------------
+
+    return timeIntegration_njit_elementwise(
+        startind,
+        t,
+        dt,
+        sqrt_dt,
+        duration,
+        N,
+        Cmat,
+        Dmat,
+        K_gl,
+        signalV,
+        coupling,
+        Dmat_ndt,
+        xs,
+        ys,
+        xs_input_d,
+        ys_input_d,
+        x_ext,
+        y_ext,
+        a,
+        w,
+        noise_xs,
+        noise_ys,
+        sigma,
+    )
+
+from numpy import sqrt
+from numpy.random import rand
+from scipy.special import erfinv
+
+
+@numba.njit
+def timeIntegration_njit_elementwise(
+    startind,
+    t,
+    dt,
+    sqrt_dt,
+    duration,
+    N,
+    Cmat,
+    Dmat,
+    K_gl,
+    signalV,
+    coupling,
+    Dmat_ndt,
+    xs,
+    ys,
+    xs_input_d,
+    ys_input_d,
+    x_ext,
+    y_ext,
+    a,
+    w,
+    noise_xs,
+    noise_ys,
+    sigma,
+):
+    ### integrate ODE system:
+    for i in range(startind, startind + len(t)):
+
+        # loop through all the nodes
+        for no in range(N):
+
+            # To save memory, noise is saved in the activity array
+            noise_xs[no] = xs[no, i]
+            noise_ys[no] = ys[no, i]
+
+            # delayed input to each node
+            xs_input_d[no] = 0
+            ys_input_d[no] = 0
+
+            # diffusive coupling
+            if coupling == 0:
+                for l in range(N):
+                    xs_input_d[no] += K_gl * Cmat[no, l] * (xs[l, i - Dmat_ndt[no, l] - 1] - xs[no, i - 1])
+                    ys_input_d[no] += K_gl * Cmat[no, l] * (ys[l, i - Dmat_ndt[no, l] - 1] - ys[no, i - 1])
+            # additive coupling
+            elif coupling == 1:
+                for l in range(N):
+                    xs_input_d[no] += K_gl * Cmat[no, l] * (xs[l, i - Dmat_ndt[no, l] - 1])
+                    ys_input_d[no] += K_gl * Cmat[no, l] * (ys[l, i - Dmat_ndt[no, l] - 1])
+
+            # Stuart-Landau / Hopf Oscillator
+            x_rhs = (
+                (a[no] - xs[no, i - 1] ** 2 - ys[no, i - 1] ** 2) * xs[no, i - 1]
+                - w[no] * ys[no, i - 1]
+                + xs_input_d[no]  # input from other nodes
+                + x_ext[no, i-1]  # external input
+            )
+            y_rhs = (
+                (a[no] - xs[no, i - 1] ** 2 - ys[no, i - 1] ** 2) * ys[no, i - 1]
+                + w[no] * xs[no, i - 1]
+                + ys_input_d[no]  # input from other nodes
+                + y_ext[no, i-1]  # external input
+            )
+
+            # Euler integration (added + np.sqrt(dt) * sigma_ou)
+            xs[no, i] = xs[no, i - 1] + dt * x_rhs + np.sqrt(dt) * sigma * np.random.randn() # mV/ms
+            ys[no, i] = ys[no, i - 1] + dt * y_rhs + np.sqrt(dt) * sigma * np.random.randn()  # mV/ms
+
+
+    return t, xs, ys, np.nan, np.nan
